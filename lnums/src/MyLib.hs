8b5944421532be6631e8baf8191bdaef47736fff
Penny Chase <mpchase@mac.com>
2024-10-12 17:26:05 -0400

    Original version of numberAlLines

--- lnums/src/MyLib.hs
+++ lnums/src/MyLib.hs
@@ -1,4 +1,11 @@
-module MyLib (someFunc) where
+module MyLib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type NumberedLine = (Maybe Int, String)
+type NumberedLines = [NumberedLine]
+
+numberAllLines :: [String] -> NumberedLines
+numberAllLines lines =
+    let go :: Int -> [String] -> NumberedLines
+        go _ [] = []
+        go counter (x : xs) = (Just counter, x) : go (counter + 1) xs
+    in go 1 lines
