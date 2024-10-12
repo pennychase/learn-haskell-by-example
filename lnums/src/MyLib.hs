dfe5e2f848ee2d9cec57cbd85e0d3068b1c5b29e
Penny Chase <mpchase@mac.com>
2024-10-12 18:03:42 -0400

    Generalized line numbering

--- lnums/src/MyLib.hs
+++ lnums/src/MyLib.hs
@@ -1,11 +1,38 @@
 module MyLib where
 
+import Data.Char
+
 type NumberedLine = (Maybe Int, String)
 type NumberedLines = [NumberedLine]
 
-numberAllLines :: [String] -> NumberedLines
-numberAllLines lines =
+-- predicates to test if a string contains at least one printable character, or not
+isEmpty :: String -> Bool
+isEmpty str =
+    null str
+        || all (\s -> not (isPrint s) || isSeparator s) str
+
+notIsEmpty :: String -> Bool
+notIsEmpty = not . isEmpty
+
+-- generalized higher-order function for nunbering lines
+numberLines :: (String -> Bool) -> (String -> Bool) -> [String] -> NumberedLines
+numberLines shouldIncr shouldNumber text =
     let go :: Int -> [String] -> NumberedLines
         go _ [] = []
-        go counter (x : xs) = (Just counter, x) : go (counter + 1) xs
-    in go 1 lines
+        go counter (x : xs) = 
+            let mNumbering = if shouldNumber x then (Just counter) else Nothing
+                newCounter = if shouldIncr x then counter + 1 else counter
+            in (mNumbering, x) : go newCounter xs
+    in go 1 text
+
+-- number all lines
+numberAllLines :: [String] -> NumberedLines
+numberAllLines = numberLines (const True) (const True)
+
+-- number non-empty lines but increment counter for all lines
+numberNonEmptyLines :: [String] -> NumberedLines
+numberNonEmptyLines = numberLines (const True) notIsEmpty
+
+-- number non-empty lines without incrementing the counter for empty lines
+numberAndIncrementNonEmptyLines :: [String] -> NumberedLines
+numberAndIncrementNonEmptyLines = numberLines notIsEmpty notIsEmpty
