056b42e2258c122c086f90e2ffbcd29527403eb2
Penny Chase <mpchase@mac.com>
2024-10-14 19:27:40 -0400

    Completed lnums

--- lnums/app/Main.hs
+++ lnums/app/Main.hs
@@ -1,17 +1,37 @@
 module Main (main) where
 
+import Data.Maybe
 import System.Environment
 import MyLib
+
+data LineNumberOption
+  = ReverseNumbering
+  | SkipEmptyLines
+  | LeftAlign
+  deriving (Eq, Show)
+
+lnOptionFromString :: String -> Maybe LineNumberOption
+lnOptionFromString "--reverse" = Just ReverseNumbering
+lnOptionFromString "--skip-empty" = Just SkipEmptyLines
+lnOptionFromString "--left-align" = Just LeftAlign
+lnOptionFromString _ = Nothing
 
 printHelpText :: String -> IO ()
 printHelpText msg = do
   putStrLn (msg ++ "\n")
   progName <- getProgName
-  putStrLn ("Usage: " ++ progName ++ " <filename>")
+  putStrLn ("Usage: " ++ progName ++ " <options> <filename>")
+  putStrLn "\n"
+  putStrLn "Options: "
+  putStrLn "   --reverse       - Reverse the numbering"
+  putStrLn "   --skip-empty    - Skip numbering empty lines"
+  putStrLn "   --left-align    - Use left-aligned line numbers"
 
-parseArguments :: [String] -> Maybe FilePath
-parseArguments [filepath] = Just filepath
-parseArguments _ = Nothing
+parseArguments :: [String] -> (Maybe FilePath, [LineNumberOption])
+parseArguments args =
+  case reverse args of
+    [] -> (Nothing, [])
+    filename : options -> (Just filename, mapMaybe lnOptionFromString options)
 
 readLines :: FilePath -> IO [String]
 readLines filepath = do
@@ -21,16 +41,29 @@ readLines filepath = do
 main :: IO ()
 main = do
   cliArgs <- getArgs
-  let mFilePath = parseArguments cliArgs
-  maybe 
-    (printHelpText "Missing filename")
-    (\filePath -> do
+
+  let (mFilePath, options) = parseArguments cliArgs
+
+      numberFunction
+        | SkipEmptyLines `elem` options = numberNonEmptyLines
+        | otherwise = numberAllLines
+
+      padMode 
+        | LeftAlign `elem` options = PadRight
+        | otherwise = PadLeft
+
+      go filePath = do
         fileLines <- readLines filePath
-        let numbered = numberAllLines fileLines
-            prettyNumbered = prettyNumberedLines PadLeft numbered
-        mapM_ putStrLn prettyNumbered
-    )
+        let numbered = numberFunction fileLines
+            prettyNumbered = prettyNumberedLines padMode numbered
+            revNumbered = numberFunction (reverse fileLines)
+            revPretty = reverse (prettyNumberedLines padMode revNumbered)
+        mapM_ putStrLn (if ReverseNumbering `elem` options then revPretty else prettyNumbered)
+        
+  maybe
+    (printHelpText "Missing filename")
+    go
     mFilePath
-  
-  
+
+
 
