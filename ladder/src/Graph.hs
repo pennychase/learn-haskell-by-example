5ceca6af8b481cdc3ac504f683acfccb2fe238ed
Penny Chase <mpchase@mac.com>
2024-10-30 19:23:16 -0400

    Implemented PermutationMap

--- ladder/src/Graph.hs
+++ ladder/src/Graph.hs
@@ -23,3 +23,12 @@ buildDiGraph = foldr (\(k, v) xs -> M.insert k v xs) M.empty
 
 children :: (Eq a) => a -> DiGraph a -> [a]
 children = M.findWithDefault []
+
+deleteNode :: (Eq a) => a -> DiGraph a -> DiGraph a
+deleteNode = M.delete
+
+deleteEdge :: (Eq a) => (a, a) -> DiGraph a -> DiGraph a
+deleteEdge (node, node') graph = M.alter deleteEdge' node graph
+    where
+        deleteEdge' Nothing = Nothing
+        deleteEdge' (Just nodes) = Just (L.delete node' nodes)
