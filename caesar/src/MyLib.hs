2f037ecca330f7d4104227bbe7935b82e557c234
Penny Chase <mpchase@mac.com>
2025-12-18 06:39:20 -0500

    Caesar cipher data types and helper functions

---
+++ caesar/src/MyLib.hs
@@ -0,0 +1,28 @@
+module MyLib where
+
+type Alphabet = [Char]
+
+lowerAlphabet :: Alphabet
+lowerAlphabet = ['a' .. 'z']
+
+upperAlphabet :: Alphabet
+upperAlphabet = ['A' .. 'Z']
+
+digits :: Alphabet
+digits = ['0' .. '9']
+
+isLower :: Char -> Bool
+isLower char = char `elem` lowerAlphabet
+
+isUpper :: Char -> Bool
+isUpper char = char `elem` upperAlphabet
+
+isDigit :: Char -> Bool
+isDigit char = char `elem` digits
+
+isMisc :: Char -> Bool
+isMisc char = char `notElem` lowerAlphabet ++ upperAlphabet ++ digits
+
+indexOf :: Char -> Alphabet -> Int
+indexOf ch [] = undefined
+indexOf ch (x : xs) = if x == ch then 0 else 1 + indexOf ch xs
