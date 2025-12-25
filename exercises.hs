78bf0f9c24710415a8aa1896bad1c3fbc6d81829
Penny Chase <mpchase@mac.com>
2025-12-25 16:02:48 -0500

    Chapter 4 exercises

--- exercises.hs
+++ exercises.hs
@@ -22,3 +22,49 @@ indexOf ch str = go ch str 0
         go ch (x : xs) n
             | ch == x = Just n
             | otherwise = go ch xs (n + 1)
+
+
+-- Chapter 4
+
+myWords :: String -> [String]
+myWords str = split str ' '
+
+myLines :: String -> [String]
+myLines str = split str '\n'
+
+split :: String -> Char -> [String]
+split str char = 
+    let go :: String -> Char -> String -> [String] -> [String]
+        go "" _ cur  acc = reverse (reverse cur : acc)
+        go (x : xs) ch cur acc 
+            | x == ch = go xs ch "" (reverse cur : acc)
+            | otherwise = go xs ch (x : cur) acc
+    in go str char "" []
+
+myUnLines :: [String] -> String
+myUnLines strs = join strs "\n"
+
+myUnWords :: [String] -> String
+myUnWords wrds = join wrds " "
+
+join :: [String] -> String -> String
+join [] _ = ""
+join (x : xs) s 
+    | null xs = x
+    | otherwise = x ++ s ++ join xs s
+
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x : xs) (y : ys) = (x, y) : (myZip xs ys)
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _f [] _ = []
+myZipWith _f _ [] = []
+myZipWith f (x : xs) (y : ys) = (f x y) : (myZipWith f xs ys)
+
+myZip' :: [a] -> [b] -> [(a, b)]
+myZip' = myZipWith (,)
+
+
