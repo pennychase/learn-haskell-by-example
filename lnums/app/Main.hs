ff21588ba5802241765a467f4eeb72d3b27bbe7e
Penny Chase <mpchase@mac.com>
2025-12-27 11:17:49 -0500

    Chapter 4 - exercises for lnums (added options)

--- lnums/app/Main.hs
+++ lnums/app/Main.hs
@@ -1,5 +1,6 @@
 module Main(main) where
 
+import Control.Monad
 import Data.Maybe
 import MyLib
 import System.Environment
@@ -7,13 +8,17 @@ import System.Environment
 data LineNumberOption 
   = ReverseNumbering
   | SkipEmptyLines
+  | SkipEmptyIncrement
   | LeftAlign
+  | HelpText
   deriving (Eq, Show)
 
 lnOptionFromString :: String -> Maybe LineNumberOption 
 lnOptionFromString "--reverse" = Just ReverseNumbering
 lnOptionFromString "--skip-empty" = Just SkipEmptyLines
+lnOptionFromString "--skip-empty-incr" = Just SkipEmptyIncrement
 lnOptionFromString "--left-align" = Just LeftAlign
+lnOptionFromString "--help" = Just HelpText
 lnOptionFromString _ = Nothing
 
 printHelpText :: String -> IO ()
@@ -23,13 +28,16 @@ printHelpText msg = do
   putStrLn ("Usage: " ++ progName ++ " <options> <filename>")
   putStrLn "\n"
   putStrLn " Options:"
-  putStrLn "    -- reverse      - Reverse the numbering"
-  putStrLn "    -- skip-empty   - Skip numbering empty lines"
-  putStrLn "    -- left-align   - Use left-aligned line numbers"
+  putStrLn "    --reverse           - Reverse the numbering"
+  putStrLn "    --skip-empty        - Skip numbering empty lines"
+  putStrLn "    --skip-empty-incr   - Skip numbering empty lines and increment"
+  putStrLn "    --left-align        - Use left-aligned line numbers"
+  putStrLn "    --help              - Print this message"
 
 parseArguments :: [String] -> (Maybe FilePath, [LineNumberOption])
 parseArguments args = case reverse args of
   [] -> (Nothing, [])
+  ["--help"] -> (Nothing, [HelpText])
   (filename : options) -> (Just filename, mapMaybe lnOptionFromString options)
 
 
@@ -38,15 +46,16 @@ readLines filePath = do
   contents <- readFile filePath
   return (lines contents)
 
-
 main :: IO ()
 main = do
   cliArgs <- getArgs
   let (mFilePath, options) = parseArguments cliArgs
-      numberFunction =
-        if SkipEmptyLines `elem` options
-          then numberNonEmptyLines
-          else numberAllLines
+  when (HelpText `elem` options) (printHelpText "")
+
+  let numberFunction
+        | SkipEmptyLines `elem` options = numberNonEmptyLines
+        | SkipEmptyIncrement `elem` options = numberAndIncrementNonEmptyLines 
+        | otherwise = numberAllLines
       padMode =
         if LeftAlign `elem` options
           then PadRight
