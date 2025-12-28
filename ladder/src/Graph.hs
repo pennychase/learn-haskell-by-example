c4a231ad3ee2d8b51cb87c911a528fc2655be1b8
Penny Chase <mpchase@mac.com>
2025-12-28 09:36:17 -0500

    Chapter 5 initial Graph module

--- ladder/src/Graph.hs
+++ ladder/src/Graph.hs
@@ -1,1 +1,16 @@
 module Graph where
+
+import qualified Data.AssocMap as M
+
+type DiGraph a = M.AssocMap a [a]
+
+empty :: DiGraph a
+empty = M.empty
+
+hasNode :: Eq a => DiGraph a -> a -> Bool
+hasNode = flip M.member
+
+addNode :: Eq a => DiGraph a -> a -> DiGraph a
+addNode graph node
+    | graph `hasNode` node = graph
+    | otherwise = M.insert node [] graph
