b1e86bf6966ce1c97557672409c36eb5f862d944
Penny Chase <pc@mitre.org>
2025-12-29 17:30:34 -0500

    Chapter 6 - BFS with AssocMap

--- ladder/src/Graph.hs
+++ ladder/src/Graph.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Graph where
 
 import qualified Data.AssocMap as M
@@ -18,6 +20,10 @@ addNode graph node
 
 deleteNode :: (Eq a) => DiGraph a -> a -> DiGraph a
 deleteNode graph node = M.delete node graph
+
+deleteNodes :: (Eq a) => [a] -> DiGraph a -> DiGraph a
+deleteNodes [] graph = graph
+deleteNodes (x : xs ) graph = M.delete x (deleteNodes xs graph)
 
 addEdge :: (Eq a) => (a, a) -> DiGraph a -> DiGraph a
 addEdge (node, child) = M.alter insertEdge node
@@ -42,3 +48,46 @@ buildDiGraph adjacencyList = foldr addAdjacent empty adjacencyList
 
 children :: (Eq a) => a -> DiGraph a -> [a]
 children = M.findWithDefault []
+
+-- Breadth First Search
+
+type SearchState a =([a], DiGraph a, DiGraph a)  -- frontier, graph, predecessors graph
+
+data SearchResult a = Unsuccessful | Successful (DiGraph a)
+
+bfsSearch :: forall a. Eq a => DiGraph a -> a -> a -> Maybe [a]
+bfsSearch graph start end
+    | start == end = Just [start]
+    | otherwise =
+        case bfsSearch' ([start], graph, empty) of
+            Successful preds -> Just (findSolution preds)
+            Unsuccessful -> Nothing
+
+    where
+        addMultiplePredecessors :: [(a, [a])] -> DiGraph a -> DiGraph a
+        addMultiplePredecessors [] g = g
+        addMultiplePredecessors ((n, ch) : xs) g =
+            addMultiplePredecessors xs (go n ch g)
+            where
+                go n [] g = g
+                go n (x : xs) g = go n xs (addEdge (x, n) g)
+
+        bfsSearch' :: SearchState a -> SearchResult a
+        bfsSearch' ([], _, preds) = Unsuccessful
+        bfsSearch' (frontier, g, preds) =
+            let
+                g' = deleteNodes frontier g
+                ch = L.map (\n -> (n, L.filter (`M.member` g) (children n g))) frontier
+                frontier' = L.concatMap snd ch
+                preds' = addMultiplePredecessors ch preds
+            in if end `L.elem` frontier'
+                then Successful preds'
+                else bfsSearch' (frontier', g', preds')
+
+        findSolution :: DiGraph a -> [a]
+        findSolution g = L.reverse (go end)
+            where
+                go x =
+                    case children x g of
+                        [] -> [x]
+                        (v : _) -> x : go v
